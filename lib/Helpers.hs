8e7cbbf13774acfb578ec2e6f2213beb1fc2e0d0
ritzau <tobias@ritzau.se>
2022-12-01 23:37:28 +0000

    Clean up

--- lib/Helpers.hs
+++ lib/Helpers.hs
@@ -1,4 +1,4 @@
-module Helpers(printHeader, readData) where
+module Helpers(formatInt, printHeader, padStart, readData) where
 
 import Data.Char(toUpper)
 import Paths_AdventOfCode(getDataFileName)
@@ -15,3 +15,24 @@ printHeader title = do
 readData file = do
   path <- getDataFileName file
   readFile path
+
+formatInt :: Int -> String 
+formatInt x
+  | x < 0 = '-':format' (-x)
+  | otherwise = format' x
+  where
+    format' 0 = "0"
+    format' x = 
+      let 
+        next = x `div` 1000
+        rest = x `mod` 1000
+      in
+        if next > 0 then
+          format' next ++ "," ++ padStart 3 '0' (show rest)
+        else
+          show rest
+
+padStart :: Int -> Char -> String -> String 
+padStart n c s 
+  | length s < n = padStart n c (c:s)
+  | otherwise = s
