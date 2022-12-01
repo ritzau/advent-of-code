d89b672d245a4ce8837718113c6b4bf4e801399a
ritzau <tobias@ritzau.se>
2022-12-01 23:37:28 +0000

    Clean up

--- lib/AoC22E01.hs
+++ lib/AoC22E01.hs
@@ -1,41 +1,42 @@
 module AoC22E01 (calorieCounting) where
 
 import Data.Sort (sort)
-import Helpers (printHeader, readData)
+import Helpers (formatInt, printHeader, readData)
 
 calorieCounting :: IO ()
 calorieCounting = do
   printHeader "2022 Day 1: Calorie Counting"
 
   result <- maxCaloriesOf "data-s22e01-sample.txt"
-  putStrLn ("Sample:   " ++ show result)
+  putStrLn ("Max calories sample: " ++ formatInt result)
 
   result <- maxCaloriesOf "data-s22e01.txt"
-  putStrLn ("Part 1:   " ++ show result)
+  putStrLn ("Max calories:        " ++ formatInt result)
 
   result <- top3SumOf "data-s22e01-sample.txt"
-  putStrLn ("Sample 2: " ++ show result)
+  putStrLn ("Top 3 total sample:  " ++ formatInt result)
 
   result <- top3SumOf "data-s22e01.txt"
-  putStrLn ("Part 2:   " ++ show result)
+  putStrLn ("Top 3 total:         " ++ formatInt result)
 
 maxCaloriesOf :: FilePath -> IO Int
-maxCaloriesOf file = do
-  input <- getInput file
-  return $ maxCalories input
+maxCaloriesOf = process maxCalories
 
 top3SumOf :: FilePath -> IO Int
-top3SumOf fileName = do
-  input <- getInput fileName
-  return $ top3Sum input
+top3SumOf = process top3Sum
+
+process :: ([[Int]] -> Int) -> FilePath -> IO Int
+process f file = do
+  input <- getInput file
+  return $ f input
 
 getInput :: FilePath -> IO [[Int]]
 getInput file = do
   content <- readData file
-  return (parseInput content)
+  return $ parseInput content
 
 parseInput :: String -> [[Int]]
-parseInput text = toInts (split [] (lines text))
+parseInput = toInts . split [] . lines
   where
     toInts :: [[String]] -> [[Int]]
     toInts = map $ map read
