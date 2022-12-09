b7bde4d0a5b330830300c1e985cc808d79f4643f
ritzau <tobias@ritzau.se>
2022-12-09 19:43:13 +0000

    Simplify main and prepare for S22e10

---
+++ src/AoC22/AoC22E10.hs
@@ -0,0 +1,88 @@
+module AoC22E10 (ropeBridge) where
+
+import Data.Char (intToDigit)
+import Data.List (elemIndex, nub)
+import Debug.Trace (trace, traceShowId)
+import Helpers (printHeader, printResult, readData)
+
+data Direction = GoUp | GoRight | GoDown | GoLeft
+  deriving (Show)
+
+ropeBridge :: IO ()
+ropeBridge = do
+  sampleContent <- readData "data-s22e09-sample.txt"
+  sampleContent2 <- readData "data-s22e09-sample-2.txt"
+  content <- readData "data-s22e09.txt"
+
+  let sampleInput = parseInput sampleContent
+  let sampleInput2 = parseInput sampleContent2
+  let input = parseInput content
+
+  printHeader "2022 Day 9: Rope Bridge"
+
+  printResult "Count distinct sample positions" 13 $
+    countDistinctRopePositions 2 sampleInput
+
+  printResult "Count distinct positions" 6339 $
+    countDistinctRopePositions 2 input
+
+  printResult "Count distinct rope sample positions 1" 1 $
+    countDistinctRopePositions 10 sampleInput
+
+  printResult "Count distinct rope sample positions 2" 36 $
+    countDistinctRopePositions 10 sampleInput2
+
+  printResult "Count distinct rope positions" 2541 $
+    countDistinctRopePositions 10 input
+
+countDistinctRopePositions :: Int -> [Direction] -> Int
+countDistinctRopePositions n input =
+  length $ traceRope $ nub $ map last $ process (replicate n (0, 0)) input
+  where
+    process :: [(Int, Int)] -> [Direction] -> [[(Int, Int)]]
+    process (k : ks) (d : ds) =
+      let rope = moveRope (moveHead k d : ks)
+       in rope : process rope ds
+    process _ _ = []
+
+    moveRope (k : k' : ks) = k : moveRope (moveKnot k k' : ks)
+    moveRope [k] = [k]
+    moveRope [] = undefined
+
+    moveHead (x, y) d = case d of
+      GoUp -> (x, y + 1)
+      GoRight -> (x + 1, y)
+      GoDown -> (x, y - 1)
+      GoLeft -> (x -1, y)
+
+    moveKnot h@(hx, hy) t@(tx, ty)
+      | isTouching h t = t
+      | otherwise = (tx + signum (hx - tx), ty + signum (hy - ty))
+
+    isTouching (hx, hy) (tx, ty) =
+      abs (hx - tx) <= 1 && abs (hy - ty) <= 1
+
+traceRope r = r -- trace (renderRope 20 10 r) r
+
+renderRope w h r =
+  unlines $ reverse $ map generateLine [- h .. h]
+  where
+    generateLine r = [marker r c | c <- [- w .. w]]
+    marker x y = maybe '.' marker' ((y, x) `elemIndex` r)
+    marker' x = intToDigit (x `mod` 16)
+
+parseInput :: String -> [Direction]
+parseInput = concatMap processLine . lines
+  where
+    processLine = processLine' . words
+
+    processLine' [d, n] = toMove d $ read n
+    processLine' _ = undefined
+
+    toMove d n =
+      replicate n $ case d of
+        "U" -> GoUp
+        "R" -> GoRight
+        "D" -> GoDown
+        "L" -> GoLeft
+        _ -> undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,25 @@
 module Main where
 
-import AoC21E01 (sonarSweep)
-import AoC22E01 (calorieCounting)
-import AoC22E02 (rockPaperScissors)
-import AoC22E03 (rucksackReorganization)
-import AoC22E04 (campCleanup)
-import AoC22E05 (supplyStacks)
-import AoC22E06 (tuningTrouble)
-import AoC22E07 (noSpaceLeftOnDevice)
-import AoC22E08 (treetopTreeHouse)
-import AoC22E09 (ropeBridge)
-
-data Episode
-  = S21E01
-  | S22E01
-  | S22E02
-  | S22E03
-  | S22E04
-  | S22E05
-  | S22E06
-  | S22E07
-  | S22E08
-  | S22E09
-
-runEpisode e = case e of
-  S21E01 -> sonarSweep
-  S22E01 -> calorieCounting
-  S22E02 -> rockPaperScissors
-  S22E03 -> rucksackReorganization
-  S22E04 -> campCleanup
-  S22E05 -> supplyStacks
-  S22E06 -> tuningTrouble
-  S22E07 -> noSpaceLeftOnDevice
-  S22E08 -> treetopTreeHouse
-  S22E09 -> ropeBridge
+import qualified AoC22E01 (calorieCounting)
+import qualified AoC22E02 (rockPaperScissors)
+import qualified AoC22E03 (rucksackReorganization)
+import qualified AoC22E04 (campCleanup)
+import qualified AoC22E05 (supplyStacks)
+import qualified AoC22E06 (tuningTrouble)
+import qualified AoC22E07 (noSpaceLeftOnDevice)
+import qualified AoC22E08 (treetopTreeHouse)
+import qualified AoC22E09 (ropeBridge)
+import qualified AoC22E10 (ropeBridge)
 
 main :: IO ()
-main =
-  runEpisodes
-    [ S22E01,
-      S22E02,
-      S22E03,
-      S22E04,
-      S22E05,
-      S22E06,
-      S22E07,
-      S22E08,
-      S22E09
-    ]
-  where
-    runEpisodes (e : es) = do
-      runEpisode e
-      runEpisodes es
-    runEpisodes _ = return ()
+main = do
+  AoC22E01.calorieCounting
+  AoC22E02.rockPaperScissors
+  AoC22E03.rucksackReorganization
+  AoC22E04.campCleanup
+  AoC22E05.supplyStacks
+  AoC22E06.tuningTrouble
+  AoC22E07.noSpaceLeftOnDevice
+  AoC22E08.treetopTreeHouse
+  AoC22E09.ropeBridge
+  AoC22E10.ropeBridge
