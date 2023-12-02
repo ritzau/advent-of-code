7f2bf7ed94811e14efe1b61181b5494981bad346
Tobias Ritzau <tobias@ritzau.se>
2023-12-02 01:46:23 +0100

    Solve AoC23E01

---
+++ src/AoC23/index.ts
@@ -0,0 +1,17 @@
+console.log("Hello, world!!")
+
+async function launch(module: string) {
+    (await require(module)).main()
+}
+
+try {
+    const episodes = [
+        "./AoC23E01",
+    ]
+
+    for (const episode of episodes) {
+        launch(episode)
+    }
+} catch (exception) {
+    console.error(exception)
+}
