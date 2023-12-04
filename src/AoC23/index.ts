58f8595f463ba873994b99fac42b4c4bbbe41855
Tobias Ritzau <tobias@ritzau.se>
2023-12-04 22:39:52 +0100

    Solve s23e04

--- src/AoC23/index.ts
+++ src/AoC23/index.ts
@@ -1,19 +1,27 @@
 console.log("Hello, world!!")
 
-async function launch(module: string) {
-    (await require(module)).main()
+async function launch(moduleName: string) {
+    const title = moduleName.replace(/.*\//, '')
+    console.log(`\n${title}`)
+    console.log('='.repeat(title.length))
+
+    const module = await require(moduleName)
+    await module.main()
 }
 
-try {
+async function main() {
     const episodes = [
-        // "./AoC23E01",
-        // "./AoC23E02",
+        "./AoC23E01",
+        "./AoC23E02",
         "./AoC23E03",
+        "./AoC23E04",
     ]
 
     for (const episode of episodes) {
-        launch(episode)
+        await launch(episode)
     }
-} catch (exception) {
-    console.error(exception)
 }
+
+main()
+    .then(() => { console.log("\nDone")})
+    .catch(e => console.error("\nUnexpected error", e))
