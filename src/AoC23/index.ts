78bcf3970afa2fcf803a7fa6d1e73c717de54cb4
Tobias Ritzau <tobias@ritzau.se>
2023-12-23 13:47:53 +0100

    Solve AoC23E19

--- src/AoC23/index.ts
+++ src/AoC23/index.ts
@@ -1,60 +1,60 @@
-import assert from "node:assert"
+import assert from "node:assert";
 
-const runAllTests = false
+const runAllTests = false;
 
 async function main() {
     for (const episode of episodes()) {
-        if (!await launch(episode)) break
+        if (!await launch(episode)) break;
     }
 }
 
 function episodes(start: number | undefined = undefined, end: number | undefined = undefined) {
     if (start === undefined && end === undefined) {
-        return episodeRange(1, 25)
+        return episodeRange(1, 25);
     }
 
     if (start !== undefined && end !== undefined) {
-        return episodeRange(start, end)
+        return episodeRange(start, end);
     }
 
     if (start !== undefined && end === undefined) {
-        return episodeRange(start, start)
+        return episodeRange(start, start);
     }
 
     if (start === undefined && end !== undefined) {
-        return episodeRange(1, end)
+        return episodeRange(1, end);
     }
 
-    assert.fail()
+    assert.fail();
 }
 
 function episodeRange(start: number, end: number) {
     return Array.from({ length: end - start + 1 }, (_, k) =>
-        `./AoC23E${(k + start).toString().padStart(2, '0')}`)
+        `./AoC23E${(k + start).toString().padStart(2, '0')}`);
 }
 
 async function launch(moduleName: string) {
-    console.log()
-    const title = moduleName.replace(/.*\//, '')
-    console.group(title)
-    console.time(title)
+    console.log();
+    const title = moduleName.replace(/.*\//, '');
+    console.group(title);
+    console.time(title);
     try {
-        const module = await require(moduleName)
-        await module.main(runAllTests)
-        return true
+        const module = await require(moduleName);
+        await module.main(runAllTests);
+        return true;
     } catch (error) {
         if (error instanceof Error && error.message.startsWith("Cannot find module")) {
-            console.log("NYI")
-            return false
+            console.log("NYI");
+            return false;
         } else {
-            throw error
+            throw error;
         }
     } finally {
-        console.timeEnd(title)
-        console.groupEnd()
+        console.timeEnd(title);
+        console.groupEnd();
     }
 }
 
 main()
-    .then(() => { console.log("\nDone") })
-    .catch(e => console.error("\nUnexpected error", e))
+    .then(() => { console.log("\nDone"); })
+    .catch(e => console.error("\nUnexpected error", e));
