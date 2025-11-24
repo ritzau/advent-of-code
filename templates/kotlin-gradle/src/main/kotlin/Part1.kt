af2448beea1fdba942db363aa2c2aa85a93d4f4b
Tobias Ritzau <tobias@ritzau.se>
2025-11-24 22:49:19 +0100

    Remove redundant flakes and refs

--- templates/kotlin-gradle/src/main/kotlin/Part1.kt
+++
@@ -1,7 +0,0 @@
-// Advent of Code - Part 1 executable
-
-fun main() {
-    val input = generateSequence(::readLine).joinToString("\n")
-    val result = solvePart1(input)
-    println(result)
-}
--- templates/kotlin-gradle/src/main/kotlin/Part2.kt
+++
@@ -1,7 +0,0 @@
-// Advent of Code - Part 2 executable
-
-fun main() {
-    val input = generateSequence(::readLine).joinToString("\n")
-    val result = solvePart2(input)
-    println(result)
-}
