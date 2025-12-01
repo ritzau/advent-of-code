e6220978c3632987ffc66717cab3003934b92d6c
Tobias Ritzau <tobias@ritzau.se>
2025-12-01 23:52:56 +0100

    Add aliases for the dev container

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,28 @@
+alias bb="bazel build ..."
+alias bba="bazel build //..."
+alias bt="bazel test ..."
+alias bta="bazel test //..."
+alias aoc="bazel run //:aoc --"
+alias bf="bazel run //tools:format"
+alias gr="git rev-parse --show-toplevel"
+alias cgr="cd $(git rev-parse --show-toplevel)"
+alias gcan!='git commit --all --amend --no-edit'
+alias gcof='git checkout "$(git branch --format=\"%(refname:short)" | fzf)"'
+alias gd='git diff'
+alias gds='git diff --stat'
+alias gdt='git difftool --dir-diff --no-symlinks'
+alias gdtm='git difftool --dir-diff --no-symlinks origin/main'
+alias gdts='git difftool --no-prompt'
+alias gdtsm='git difftool --no-prompt origin/main'
+alias gdw='git diff --word-diff-regex=.'
+alias gf='git fetch'
+alias gl='git log --oneline --max-count=42'
+alias gl1='git log --max-count=1'
+alias glg='git log --graph --oneline --branches --max-count=24 @'
+alias glga='git log --graph --oneline --branches @'
+alias gmt='git mergetool'
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+alias gpsup='git push --set-upstream origin $(git branch --show-current)'
+alias grasq='git rebase --interactive --autosquash origin/main'
+alias gs='git status --ahead-behind'
