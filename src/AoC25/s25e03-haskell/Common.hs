b58d49d52f57e2c8438fc5e0b7940aa460368a4f
Tobias Ritzau <tobias@ritzau.se>
2025-12-04 23:24:18 +0100

    dp

--- src/AoC25/s25e03-haskell/Common.hs
+++ src/AoC25/s25e03-haskell/Common.hs
@@ -1,102 +1,55 @@
 module Common (solvePart1, solvePart2) where
 
 import Data.Char
+import Data.Array
 
-maxJolt1 :: String -> Maybe Int
-maxJolt1 (x:[]) = Just $ digitToInt x
-maxJolt1 (x:xs) =
-  let
-    digit = digitToInt x
-    tailMax = maxJolt1 xs
-  in
-    case tailMax of
-      Just v -> Just $ max digit v
-      _ -> Nothing
-maxJolt1 [] = Nothing
+maxJoltN :: Int -> String -> Maybe Int
+maxJoltN maxDigits str
+  | null str = Nothing
+  | maxDigits <= 0 = Nothing
+  | otherwise =
+    let
+      n = length str
+      digits = map digitToInt str
+      bounds = ((0, 0), (n, maxDigits))
 
-maxJolt2 :: String -> Maybe Int
-maxJolt2 (x:xs) = maxJolt 10 x (maxJolt1 xs) (maxJolt2 xs)
-maxJolt2 _ = Nothing
+      dp :: Array (Int, Int) (Maybe Int)
+      dp = array bounds [((i, j), compute i j) | i <- [0..n], j <- [0..maxDigits]]
 
-maxJolt3 :: String -> Maybe Int
-maxJolt3 (x:xs) = maxJolt 100 x (maxJolt2 xs) (maxJolt3 xs)
-maxJolt3 _ = Nothing
+      compute :: Int -> Int -> Maybe Int
+      compute pos remaining
+        | remaining == 0 = Just 0  -- Successfully selected all digits
+        | pos >= n = Nothing       -- Out of string, but still need more digits
+        | otherwise =
+          let
+            digitsLeft = n - pos
+            -- If we can't possibly select enough digits, fail early
+            canFinish = digitsLeft >= remaining
 
-maxJolt4 :: String -> Maybe Int
-maxJolt4 (x:xs) = maxJolt 1000 x (maxJolt3 xs) (maxJolt4 xs)
-maxJolt4 _ = Nothing
+            -- Option 1: Skip this digit
+            skipThis = if canFinish then dp ! (pos + 1, remaining) else Nothing
 
-maxJolt5 :: String -> Maybe Int
-maxJolt5 (x:xs) = maxJolt 10000 x (maxJolt4 xs) (maxJolt5 xs)
-maxJolt5 _ = Nothing
+            -- Option 2: Use this digit
+            -- When we use a digit, it contributes: digit * 10^(remaining - 1)
+            -- because we're selecting the (maxDigits - remaining + 1)th digit
+            useThis = case dp ! (pos + 1, remaining - 1) of
+                        Just rest -> Just $ (10 ^ (remaining - 1)) * (digits !! pos) + rest
+                        Nothing -> Nothing
+          in
+            if not canFinish
+            then Nothing
+            else case (skipThis, useThis) of
+              (Nothing, x) -> x
+              (x, Nothing) -> x
+              (Just s, Just u) -> Just $ max s u
+    in
+      dp ! (0, maxDigits)
 
-maxJolt6 :: String -> Maybe Int
-maxJolt6 (x:xs) = maxJolt 100000 x (maxJolt5 xs) (maxJolt6 xs)
-maxJolt6 _ = Nothing
-
-maxJolt7 :: String -> Maybe Int
-maxJolt7 (x:xs) = maxJolt 1000000 x (maxJolt6 xs) (maxJolt7 xs)
-maxJolt7 _ = Nothing
-
-maxJolt8 :: String -> Maybe Int
-maxJolt8 (x:xs) = maxJolt 10000000 x (maxJolt7 xs) (maxJolt8 xs)
-maxJolt8 _ = Nothing
-
-maxJolt9 :: String -> Maybe Int
-maxJolt9 (x:xs) = maxJolt 100000000 x (maxJolt8 xs) (maxJolt9 xs)
-maxJolt9 _ = Nothing
-
-maxJolt10 :: String -> Maybe Int
-maxJolt10 (x:xs) = maxJolt 1000000000 x (maxJolt9 xs) (maxJolt10 xs)
-maxJolt10 _ = Nothing
-
-maxJolt11 :: String -> Maybe Int
-maxJolt11 (x:xs) = maxJolt 10000000000 x (maxJolt10 xs) (maxJolt11 xs)
-maxJolt11 _ = Nothing
-
-maxJolt12 :: String -> Maybe Int
-maxJolt12 (x:xs) = maxJolt 100000000000 x (maxJolt11 xs) (maxJolt12 xs)
-maxJolt12 _ = Nothing
-
-maxJolt :: Int -> Char -> Maybe Int -> Maybe Int -> Maybe Int
-maxJolt f x maxJoltNext skipThis =
-  let
-    useThis = case maxJoltNext of
-      Just j -> Just $ f * (digitToInt x) + j
-      _ -> Nothing
-  in
-    case (skipThis, useThis) of
-      (Nothing, j) -> j
-      (j, Nothing) -> j
-      (Just sj, Just uj) -> Just $ max sj uj
-
-sumMaxJolts2 :: [String] -> Int
-sumMaxJolts2 [] = 0
-sumMaxJolts2 (x:xs) =
-  let
-    mj = maxJolt2 x
-    headJolt = case mj of
-      Just j -> j
-      _ -> 0
-    tailJolt = sumMaxJolts2 xs
-  in
-    headJolt + tailJolt
-
-sumMaxJolts12 :: [String] -> Int
-sumMaxJolts12 [] = 0
-sumMaxJolts12 (x:xs) =
-  let
-    mj = maxJolt12 x
-    headJolt = case mj of
-      Just j -> j
-      _ -> 0
-    tailJolt = sumMaxJolts12 xs
-  in
-    headJolt + tailJolt
-
+sumMaxJolts :: Int -> [String] -> Int
+sumMaxJolts n = sum . map (maybe 0 id . maxJoltN n)
 
 solvePart1 :: String -> Int
-solvePart1 = sumMaxJolts2 . lines
+solvePart1 = sumMaxJolts 2 . lines
 
 solvePart2 :: String -> Int
-solvePart2 = sumMaxJolts12 . lines
+solvePart2 = sumMaxJolts 12 . lines
