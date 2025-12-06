56968f16fb1568d040da00e577acd18370ef0632
Tobias Ritzau <tobias@ritzau.se>
2025-12-06 22:59:49 +0100

    Set expected values in main

--- src/AoC25/s25e06-swift/Main.swift
+++ src/AoC25/s25e06-swift/Main.swift
@@ -16,7 +16,7 @@ enum Main {
         let start1 = Date()
         let result1 = solvePart1(input)
         let duration1 = Date().timeIntervalSince(start1) * 1000
-        let expectedPart1 = 0 // TODO: Set expected value
+        let expectedPart1 = 5_977_759_036_837
         let pass1 = result1 == expectedPart1
 
         let emoji1 = pass1 ? "✅" : "❌"
@@ -26,7 +26,7 @@ enum Main {
         let start2 = Date()
         let result2 = solvePart2(input)
         let duration2 = Date().timeIntervalSince(start2) * 1000
-        let expectedPart2 = 0 // TODO: Set expected value
+        let expectedPart2 = 9_630_000_828_442
         let pass2 = result2 == expectedPart2
 
         let emoji2 = pass2 ? "✅" : "❌"
